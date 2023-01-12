e4f34c4d97e2c23a955b9f1c333989fcc90d285e
MiikaM <miika.mikkonen5@gmail.com>
2023-01-12 13:30:31 +0200

    Blog schema for sequelize

--- models/blog.js
+++ models/blog.js
@@ -1,33 +1,72 @@
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
+require('dotenv').config()
+const { Sequelize, Model, DataTypes } = require('sequelize')
 
-const blogSchema = mongoose.Schema({
-  title: {
-    type: String,
-    required: true
-  },
-  author: {
-    type: String,
-    required: true
-  },
-  url: String,
-  likes: {
-    type: Number,
-    default: 0
-  },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
-  }
-})
+const sequelize = new Sequelize(process.env.DATABASE_UR)
 
-blogSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
+// const blogSchema = mongoose.Schema({
+//   title: {
+//     type: String,
+//     required: true
+//   },
+//   author: {
+//     type: String,
+//     required: true
+//   },
+//   url: String,
+//   likes: {
+//     type: Number,
+//     default: 0
+//   },
+//   user: {
+//     type: mongoose.Schema.Types.ObjectId,
+//     ref: 'User'
+//   }
+// })
 
-const Blog = mongoose.model('Blog', blogSchema)
+// blogSchema.set('toJSON', {
+//   transform: (document, returnedObject) => {
+//     returnedObject.id = returnedObject._id.toString()
+//     delete returnedObject._id
+//     delete returnedObject.__v
+//   }
+// })
+
+// const Blog = mongoose.model('Blog', blogSchema)
+
+
+
+class Blog extends Model { }
+
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    title: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    author: {
+      type: DataTypes.TEXT,
+    },
+    url: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    likes: {
+      type: DataTypes.INTEGER,
+    }
+    // ,
+    // user: {}
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: 'note'
+  })
 
 module.exports = Blog
