4b3f5e9dce5ce8b2ba8a70bd72b4a08896c3bc6a
MiikaM <miika.mikkonen5@gmail.com>
2023-01-13 13:46:37 +0200

    + bonus

--- controllers/authors.js
+++ controllers/authors.js
@@ -1,5 +1,5 @@
 const authorsRouter = require('express').Router()
-const {  Blog } = require('../models')
+const { Blog } = require('../models')
 const { sequelize } = require('../utils/db');
 
 authorsRouter.get('/', async (request, response) => {
@@ -9,6 +9,9 @@ authorsRouter.get('/', async (request, response) => {
             'author',
             [sequelize.fn('COUNT', sequelize.col('id')), 'blogs'],
             [sequelize.fn('SUM', sequelize.col('likes')), 'likes']
+        ],
+        order: [
+            ['likes', 'DESC']
         ],
         group: "author"
     });
